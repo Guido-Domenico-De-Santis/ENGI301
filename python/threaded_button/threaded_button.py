4f1368adffe3dbd6d3fcffe2c0a68d8464a4ea84
Guido-Domenico-De-Santis <158533150+Guido-Domenico-De-Santis@users.noreply.github.com>
2024-05-02 01:03:32 -0500

    Update threaded_button.py

--- python/threaded_button/threaded_button.py
+++ python/threaded_button/threaded_button.py
@@ -3,7 +3,7 @@
 Threaded Button Driver
 --------------------------------------------------------------------------
 License:   
-Copyright 2023 - 2024 - <Your Name>
+Copyright 2023 - 2024 - Guido De Santis
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
